40a60e9599596f79a9dba4e5e7c8eba1ed908ff6
ALFAA28 <162124253+ALFAA28@users.noreply.github.com>
2025-10-21 09:55:38 +0700

    migrate

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,6 +7,12 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
+    public function index()
+    {
+        $products = Product::all();
+        return response()->json($products);
+    }
+
     public function store(Request $request)
     {
 
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,12 @@
 <?php
 
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
 Route::get('/welcome', [WelcomeController::class, 'show']);
+
+
